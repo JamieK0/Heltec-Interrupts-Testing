919ba0bbf894fa02876ba31f727de4f0ad50c30a
j14273 <jamie.kyriacou101@gmail.com>
2024-12-14 15:27:41 +1100

    light

--- Interrupts-Code/Interrupts-Code.ino
+++ Interrupts-Code/Interrupts-Code.ino
@@ -2,16 +2,24 @@ const int intPin = 17;
 const int ledPin = 45;
 int buttonState = 0;
 
+unsigned long button_time = 0;  
+unsigned long last_button_time = 0; 
+
 void IRAM_ATTR ISR() {
+    button_time = millis();
+    if (button_time - last_button_time > 250)
+    {
     digitalWrite(ledPin, HIGH);
     delay(1000);
     digitalWrite(ledPin, LOW);
+    last_button_time = button_time;
+}
 }
 
 void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
-  attachInterrupt(intPin, ISR, RISING);
+  attachInterrupt(intPin, ISR, FALLING);
   pinMode(ledPin, OUTPUT);
   pinMode(intPin, INPUT);
   digitalWrite(ledPin, HIGH);
